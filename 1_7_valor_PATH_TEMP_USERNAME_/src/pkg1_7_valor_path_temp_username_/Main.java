2d407cc417b980f8704bdc3d498abd92d1b76fd1
Mauro Raimundo dos Reis <69197927+Mauroreis100@users.noreply.github.com>
2022-07-12 14:17:45 +0200

    Nr 8, 7 e 9

---
+++ 1_7_valor_PATH_TEMP_USERNAME_/src/pkg1_7_valor_path_temp_username_/Main.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg1_7_valor_path_temp_username_;
+import java.io.File;
+/**
+ *
+ * @author mauro
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       /* String path = System.getProperty("path.name");
+        String temp = System.getProperty("temp.name"); */
+        String username = System.getProperty("user.name"); 
+        System.out.print(username);
+    }
+    
+}
