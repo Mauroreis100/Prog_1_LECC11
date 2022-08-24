20819a16ed7a9562ca5ea226e4f0f6a05702bfd9
Mauro Raimundo dos Reis <69197927+Mauroreis100@users.noreply.github.com>
2022-08-24 07:01:22 +0200

    2,8,10,16,17

---
+++ Ficha 1/resultado/src/resultado/Resultado.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package resultado;
+
+/**
+ *
+ * @author mauro
+ */
+public class Resultado {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       byte b1 = 10, b2 = 20;
+short p = 200;
+int n = 500;
+long q = 100;
+float x = 2.5f;
+double y = 5.25;
+double f=b1*q*2.f / x;
+byte m = (byte) (b1*b2);
+        System.out.println(((10 % 2) % 6 )> 5 && (3 <(2 % 2)));
+    }
+    
+}
