5ca5c8378daf5b8fb817029635f1fcd324ac1a69
Mauro Raimundo dos Reis <69197927+Mauroreis100@users.noreply.github.com>
2022-10-15 15:58:49 +0200

    Ficha 7 - Até 8

--- PROGRAMAÇÃO/Ficha Arrays/7_7QuantasVezes/src/pkg7_7quantasvezes/Main.java
+++ PROGRAMAÇÃO/Ficha Arrays/7_7QuantasVezes/src/pkg7_7quantasvezes/Main.java
@@ -2,10 +2,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package pkg7_4imparespares;
+package pkg7_7quantasvezes;
 
-import java.util.Scanner;
 import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  *
@@ -13,44 +13,40 @@ import java.util.Arrays;
  */
 public class Main {
 
+    /*Crie um método que recebe um array de inteiros a e
+    um valor inteiro x e retorna a quantidade de vezes 
+    que x aparece no array a.
+     */
     public static Scanner ler = new Scanner(System.in);
 
-    /**
-     * @param args the command line arguments
-     */
-    public static int[] preencherArray(int nrs[]) {
+    public static int preencherArray(int nrs[]) {
         for (int i = 0; i < nrs.length; i++) {
             System.out.println("Insira o " + (i + 1) + "º nr: ");
             int numero = ler.nextInt();
             nrs[i] = numero;
         }
-
-        return nrs;
-    }
-
-    public static void troca(int[] nrs) {
+        System.out.println("Digite um número que deseja saber a quantidade de ocorrências");
+        int m = ler.nextInt();
+        int qtd = 0;
         for (int i = 0; i < nrs.length; i++) {
-
-            if (nrs[i] % 2 == 0) {
-                nrs[i] = nrs[i] + 1;
-            } else {
-                nrs[i] = nrs[i] - 1;
+            if (nrs[i] == m) {
+                qtd++;
             }
         }
+        System.out.print("O número "+m+" aparece ");
 
+        return qtd;
     }
 
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        /* Crie um método que recebe um array de inteiros positivos e substitui 
-               seus elementos de valor ímpar por -1 e os pares por +1.*/
+        // TODO code application logic here
         System.out.println("Quantos numeros pretende inserir?");
         int n = ler.nextInt();
         int[] a = new int[n];
-        preencherArray(a);
-        System.out.println("Os valores eram: " + Arrays.toString(a));
-        troca(a);
-        System.out.println("E depois da troca ficou: " + Arrays.toString(a));
-
+        System.out.println( preencherArray(a)+" vezes");
     }
 
 }
