648d0094daaad79971d660adb8d44505ed545751
manueljunior01 <manueldinis@protonmail.com>
2023-05-23 22:09:48 +0200

    Alteração - criação dw uma interface

--- eclipse-workspace/PROGRAMACAO_II/Vetor/Exercicio_2/Executa.java
+++ eclipse-workspace/PROGRAMACAO_II/Vetor/Exercicio_2/Executa.java
@@ -8,15 +8,21 @@ public class Executa {
 		
 		Vector filmes = new Vector();
 		
-		
+		OperacoesFilme op = new OperacoesFilme();
 		
 		//Criação dos filmes
-		Filme filme1 = new Filme(00001,"Avatar: O Caminho da Água","Acção",2022);
-		Filme filme2 = new Filme(00010,"Top Gun: Maverick","Acção",2022);
-		Filme filme3 = new Filme(00100,"Tenet","Acção",2020);
-		Filme filme4 = new Filme(01000,"O Telefone Preto","Terror",2022);
-		Filme filme5 = new Filme(10000,"Os Inocentes","Terror",2021);
-				
-		//int tamanho=filmes.size();
+		Filme filme1 = new Filme(12345,"Avatar: O Caminho da Água","Acção",2022);
+		/*Filme filme2 = new Filme(21345,"Top Gun: Maverick","Acção",2022);
+		Filme filme3 = new Filme(31245,"Tenet","Acção",2020);
+		Filme filme4 = new Filme(23422,"O Telefone Preto","Terror",2022);
+		Filme filme5 = new Filme(24245,"Os Inocentes","Terror",2021);*/
+			
+		filmes.add(filme1);
+		/*filmes.add(filme2);
+		filmes.add(filme3);
+		filmes.add(filme4);
+		filmes.add(filme5);*/
+		
+		op.listarFilmes(filmes);
 	}
 }
--- eclipse-workspace/PROGRAMACAO_II/Vetor/Exercicio_2/Filme.java
+++ eclipse-workspace/PROGRAMACAO_II/Vetor/Exercicio_2/Filme.java
@@ -2,7 +2,7 @@ package Exercicio_2;
 
 import java.util.*;
 
-public class Filme {
+public class Filme{
 	
 	private int numeroFilme;
 	private String nomeFilme;
@@ -17,58 +17,11 @@ public class Filme {
 		this.anoLancamento=anoLancamento;
 	}
 	
-	//Verificar se um filme existe
-	public boolean verificarExistencia(String filme,Vector filmes) {
-		for(int i=0;i<filmes.size();i++) {
-			if(filmes.get(i)==filme) {
-				return true;
-			}
-		}
-		return false;
+	@Override
+	public String toString() {
+		return  "O numero do filme"+numeroFilme+"\n Nome do filme:"+nomeFilme+"\n G. do filme: "+genero+"\n "
+				+ "A. de Lançamento: "+anoLancamento;
 	}
-	
-	//Metodo para atualizar o genero
-	public void atualizarGenero(String filme,String genero,Vector filmes) {
-		if(verificarExistencia(filme, filmes)==true) {
-			for(int i=0;i<filmes.size();i++) {
-				if(((Filme)filmes.get(i)).getNomeFilme()==filme) {
-					
-				}
-			}
-		}
-	}
-	
-	//Listar filmes atraves do ano de lancamento
-	public void filmeAnoLancamento(int anoLancamento,Vector filmes) {
-		for(int i=0;i<filmes.size();i++) {
-			if((((Filme) filmes.get(i)).getAnoLancamento())==anoLancamento) {
-				System.out.println(((Filme) filmes.get(i)).getNomeFilme());
-			}
-		}
-	}
-	
-	//Devolver o nome atraves do indice fornecido
-	public void nomeAtravesIndice(int indice,Vector filmes) {
-		System.out.println(((Filme) filmes.get(indice)).getNomeFilme());
-	}
-	
-	
-	//Remover um filmes
-	public boolean removerFilme(String nomeFilme,Vector filmes) {
-		for(int i=0;i<filmes.size();i++) {
-			if((((Filme) filmes.get(i)).getNomeFilme()).equalsIgnoreCase(nomeFilme)) {
-				filmes.removeElement(filmes.get(i));
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	//Inserir um filme
-	public void inserirFilme(String nomeFilme,Vector filmes) {
-		
-	}
-	
 	
 	//Getters e Setters
 	public int getNumeroFilme() {
